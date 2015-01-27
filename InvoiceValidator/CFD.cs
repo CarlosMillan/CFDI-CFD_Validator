2cdd3a5640f680f04840314b34fc71bda68e6d08
Carlos Millán <charlyomo@gmail.com>
2015-01-27 16:36:18 -0600

    optimización

--- InvoiceValidator/CFD.cs
+++ InvoiceValidator/CFD.cs
@@ -24,45 +24,58 @@ namespace InvoiceValidator
 
 	/// <summary>
 	/// By default Schema is http://www.sat.gob.mx/cfd/2
+	/// /// <param name="xml">It can be either XML Path or XML String.</param>
 	/// </summary>
-	public CFD(string xsdpath, string xmlpath)
-	  : base(null, xsdpath, xmlpath)
+	public CFD(string xsdpath, string xml)
+	  : base(null, xsdpath, xml)
 	{
 	  _schema = CFDI_SCHEMA;
 	}
 
 	/// <summary>
 	/// By default Schema is http://www.sat.gob.mx/cfd/2 and XsdPath is cfdv32.xsd
+	/// <param name="xml">It can be either XML Path or XML String.</param>
 	/// </summary>	
-	public CFD(string xmlpath)
-	  : this(null, xmlpath) 
+	public CFD(string xml)
+	  : this(null, xml) 
 	{
 	  _xsdpath = CFDI_XSD;
 	}
 
-	public CFD(string schema, string xsdpath, string xmlpath)
-	  : base(schema, xsdpath, xmlpath) {}
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="xml">It can be either XML Path or XML String.</param>
+	public CFD(string schema, string xsdpath, string xml)
+	  : base(schema, xsdpath, xml) {}
 	#endregion
 
 	#region Public Methods
 	public void Validate()
 	{
-	  string XMLPath = _xmlpath;
-	  string CfdiTmpName = null;
-	  XmlDocument XmlTmp = new XmlDocument();
-	  XmlTmp.Load(_xmlpath);
-
-	  XmlNodeList ComplementNode = XmlTmp.GetElementsByTagName(CFDI_COMPLEMENT_NODE);
-
-	  if (ComplementNode.Count > 0)
+	  if (_schema != null && _xsdpath != null && (_xmlpath != null || _xmlstring != null))
 	  {
-		CfdiTmpName = String.Concat("/", _xmlfilename, "tmp.xml");
-		ComplementNode[0].ParentNode.RemoveChild(ComplementNode[0]);
-		XMLPath = String.Concat(WORKING_DIRECTORY, CfdiTmpName);
-		XmlTmp.Save(XMLPath);
-	  }
+		string XML = _xmlpath ?? _xmlstring;
+		string CfdiTmpName = null;
+		XmlDocument XmlTmp = new XmlDocument();
 
-	  ValidateInvoice(_schema, _xsdpath, XMLPath);
+		if (_fromfile)
+		  XmlTmp.Load(XML);
+		else
+		  XmlTmp.LoadXml(XML);
+
+		XmlNodeList ComplementNode = XmlTmp.GetElementsByTagName(CFDI_COMPLEMENT_NODE);
+
+		if (ComplementNode.Count > 0)
+		{
+		  CfdiTmpName = String.Concat("/", _xmlfilename, "tmp.xml");
+		  ComplementNode[0].ParentNode.RemoveChild(ComplementNode[0]);
+		  XML = String.Concat(WORKING_DIRECTORY, CfdiTmpName);
+		  XmlTmp.Save(XML);
+		}
+
+		ValidateInvoice(_schema, _xsdpath, XML);
+	  }
 	}
 
 	public void Generate()
